63b81a4184e27bcb5f9470c1b9e8f1de14694040
Filipe <filipeasm18@gmail.com>
2022-11-08 17:29:32 -0300

    Feat: Relational data and Migrations impl

--- src/courses/course.service.ts
+++ src/courses/course.service.ts
@@ -4,35 +4,49 @@ import { Repository } from 'typeorm';
 import { CreateCourseDTO } from './dto/create-course-dto';
 import { UpdateCourseDTO } from './dto/update-course-dto';
 import { Course } from './entities/course.entity';
+import { Tag } from './entities/tag.entity';
 
 @Injectable()
 export class CourseService {
   constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
   ) {}
 
   findAll() {
-    return this.courseRepository.find();
+    return this.courseRepository.find({ relations: ['tags'] });
   }
 
   findOne(id: string) {
-    const course = this.courseRepository.findOne(id);
+    const course = this.courseRepository.findOne(id, { relations: ['tags'] });
 
     if (!course) throw new NotFoundException(`Course ${id} not found!`);
 
     return course;
   }
 
-  create(dto: CreateCourseDTO) {
-    const course = this.courseRepository.create(dto);
+  async create(dto: CreateCourseDTO) {
+    const tags = await Promise.all(
+      dto.tags.map((name) => this.preloadTagByName(name)),
+    );
+
+    const course = this.courseRepository.create({ ...dto, tags });
+
     return this.courseRepository.save(course);
   }
 
   async update(id: string, dto: UpdateCourseDTO) {
+    const tags =
+      dto.tags &&
+      (await Promise.all(dto.tags.map((name) => this.preloadTagByName(name))));
+
     const course = await this.courseRepository.preload({
       id: +id,
       ...dto,
+      tags,
     });
 
     if (!course) throw new NotFoundException(`Course ${id} not found!`);
@@ -46,5 +60,13 @@ export class CourseService {
     if (!course) throw new NotFoundException(`Course ${id} not found!`);
 
     return this.courseRepository.remove(course);
+  }
+
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOne({ name });
+
+    if (tag) return tag;
+
+    return this.tagRepository.create({ name });
   }
 }
--- src/courses/entities/course.entity.ts
+++ src/courses/entities/course.entity.ts
@@ -1,5 +1,13 @@
 /* eslint-disable prettier/prettier */
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Tag } from './tag.entity';
+
 @Entity('courses')
 export class Course {
   @PrimaryGeneratedColumn()
@@ -11,6 +19,7 @@ export class Course {
   @Column()
   description: string;
 
-  @Column('json', { nullable: true })
-  tags: string[];
+  @JoinTable()
+  @ManyToMany(() => Tag, (tag) => tag.courses, { cascade: true })
+  tags: Tag[];
 }
